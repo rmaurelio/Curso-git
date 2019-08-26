471199c2de3f79cff3a9ccbba0919a7609f2e7a2
Marcos Rodrigues <rmaurelio@gmail.com>
2019-08-26 11:35:10 -0300

    Adicao de Linhas.

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -6,11 +6,12 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como Você Está Hoje?");
             Console.WriteLine("Bom dia!");
             Console.ReadLine();
-
+            Console.WriteLine("--------------------------------------------------------");
 
 
         }
